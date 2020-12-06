3c40b9d8b95c8b8ffaae6edfa4c4e39a3e05f88d
Francesco Vetere <francescovetere1997@gmail.com>
2020-12-06 15:40:00 +0100

    lab6: update ex1-SAD

--- lab6/ex1/ex1.cpp
+++ lab6/ex1/ex1.cpp
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <cmath> // abs()
 
 struct ArgumentList {
 	std::string image_L;
@@ -16,11 +17,11 @@ struct ArgumentList {
 
 bool ParseInputs(ArgumentList& args, int argc, char **argv) {
 
-	// if(argc<3 || (argc==2 && std::string(argv[1]) == "--help") || (argc==2 && std::string(argv[1]) == "-h") || (argc==2 && std::string(argv[1]) == "-help"))
-	// {
-	// 	std::cout<<"usage: ./ex1 -i <image_L> <image_R> -w <w_size>"<<std::endl;
-	// 	return false;
-	// }
+	if(argc<6 || (argc==2 && std::string(argv[1]) == "--help") || (argc==2 && std::string(argv[1]) == "-h") || (argc==2 && std::string(argv[1]) == "-help"))
+	{
+		std::cout<<"usage: ./ex1 -i <image_L> <image_R> -w <w_size>"<<std::endl;
+		return false;
+	}
 
 	int i = 1;
 	while(i<argc)
@@ -49,17 +50,40 @@ const int max_range = 128;
 void SAD_Disparity(const cv::Mat& L, const cv::Mat& R, unsigned short w_size, cv::Mat& out) {
 	out = cv::Mat::zeros(L.rows, L.cols, CV_8UC1);
 
-	for(int row_L = 0; row_L < L.rows; ++row_L) {
-		for(int col_L = 0; col_L < L.cols; ++col_L) {
+	int current_disparity; // ad ogni valore di offset, conterrà la disparità corrente calcolata
+	int best_disparity; // ad ogni valore di offset, conterrà la migliore disparità calcolata fin' ora per (row_L, col_L)
+
+	for(int row_L = 0; row_L < L.rows - w_size; ++row_L) { // tengo conto della window
+		for(int col_L = 0; col_L < L.cols - w_size - max_range; ++col_L) { // tengo conto della window e dell'offset massimo
+			best_disparity = std::numeric_limits<int>::max();
+			
 			for(int offset = 0; offset < max_range; ++offset) {
+				current_disparity = 0;
+
 				for(int row_window = 0; row_window < w_size; ++row_window) {
 					for(int col_window = 0; col_window < w_size; ++col_window) {
-						// TODO
+
+						current_disparity += std::abs(
+							L.at<unsigned char>(row_L + row_window, col_L + col_window) -
+							// faccio la ricerca sulla stessa riga!
+							R.at<unsigned char>(row_L + row_window, col_L + col_window + offset)
+						);
+
+						if(current_disparity < best_disparity) 
+							best_disparity = current_disparity; // aggiorno l'attuale valore migliore di disparità
 					}
 				}
 			}
+
+			// Terminati i confronti a tutti gli offset possibili, ho in best_disparity il valore da assegnare a out(row_L, col_L)
+			out.at<unsigned char>(row_L, col_L) = best_disparity;
 		}
 	}
+
+	double min, max;
+	cv::minMaxLoc(out, &min, &max);
+
+	255*(out-min) / (max-min);
 }
 
 int main(int argc, char **argv) {
@@ -79,14 +103,15 @@ int main(int argc, char **argv) {
 	while(!exit_loop) {
 		cv::Mat image_L = cv::imread(args.image_L);
 		cv::Mat image_R = cv::imread(args.image_R);
+		unsigned short w_size = args.w_size;
 
 		cv::Mat disparity;
 		
 		//////////////////////
 		//processing code here
 
-		std::cout << "w_size: " << args.w_size << std::endl;
-		SAD_Disparity(image_L, image_R, 7, disparity);
+		std::cout << "w_size: " << w_size << std::endl;
+		SAD_Disparity(image_L, image_R, w_size, disparity);
 
 		/////////////////////
 
@@ -97,9 +122,10 @@ int main(int argc, char **argv) {
 		cv::namedWindow("image_R", cv::WINDOW_AUTOSIZE);
 		cv::imshow("image_R", image_R);
 
-		cv::namedWindow("disparity", cv::WINDOW_AUTOSIZE);
-		cv::imshow("disparity", disparity);
+		cv::namedWindow("disparity (w_size: " + std::to_string(w_size) + ")", cv::WINDOW_AUTOSIZE);
+		cv::imshow("disparity (w_size: " + std::to_string(w_size) + ")", disparity);
 
+		cv::imwrite("disparity (w_size: " + std::to_string(w_size) + ").jpg", disparity);
 		//wait for key or timeout
 		unsigned char key = cv::waitKey(0);
 		std::cout<<"key "<<int(key)<<std::endl;
